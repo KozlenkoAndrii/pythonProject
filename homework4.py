ba812c8f9fe07eed00e4ba4750566d44bdcca522
Andrii Kozlenko <mister.andrey7777@gmail.com>
2021-12-29 16:43:35 +0200

    homework4.py

--- homework4.py
+++ homework4.py
@@ -1,3 +1,11 @@
+''' Задание №1 Доделать, довести до ума, сделать красиво/просто/элегантно (нужное подчеркнуть) то, что делали на практике.
+Копировать код с лекции не обязательно :р (Напоминаю условия: email - имя из букв длиной не менее 3 символов,
+одна собака, доменное имя из двух частей через точку, обе части из букв длинной не менее 3 символов; телефон -
+либо в формате +380121234567 либо 0121234567, дефисы, скобки пробелы - разрешены) '''
+
+'''Задание №2 Добавить к проверке строки проверку на ФИО (Требования: длина не менее 2 символа для каждого слова,
+              каждое слово с большой буквы, в слове только буквы, например Иванов Иван Иванович)'''
+
 user_input = input('enter here: ')
 
 user_input = user_input.strip()
@@ -8,7 +16,6 @@ signs = '()- '
 
 if ('@' in user_input) and ('.' in user_input):
     raw_list = user_input.split('@')
-    # print(raw_list)
 
     if len(raw_list) != 2:
         pass
@@ -31,53 +38,68 @@ else:
     ]):
         res = 'Phone'
 
+    else:
+        for raw_list1 in user_input.split():
+            if raw_list1[0].isalpha() and raw_list1[0].isupper() and len(raw_list1) > 2:
+                res = "Name"
+
 if res:
     print(f'\"{user_input}\" is {res}')
-raw_list1 = user_input
-if
-if raw_list1.isalpha() and raw_list1[0].isupper and len(raw_list1) > 2:
-    res = "Name"
+
 else:
     print('WHAT?????')
 
-# user_input = input('enter here: ')
-#
-# user_input = user_input.strip()
-#
-# res = ''  # переменная отвечающая за результат.
-#
-# signs = '()- '
-#
-# if ('@' in user_input) and ('.' in user_input):  # если собака в пользователе и точка в пользователе
-#     raw_list = user_input.split('@') # мы разделяем по собаке
-#     print(raw_list) #
-#
-#     if len(raw_list) != 2: # если длина не равно 2
-#         pass    # то мы пропускаем
-#     else:  # в противном случае
-#         if raw_list[0].isalpha() and len(raw_list[0]) > 2: # если начало состоит из букв и длина больше 2
-#             scnd_raw = raw_list[1].split('.')   # разделить по точке
-#             if (scnd_raw[0].isalpha() and scnd_raw[1].isalpha()) and (len(scnd_raw[0]) > 2 and len(scnd_raw[1]) > 2):
-#  # если scnd_raw[0].isalpha и scnd_raw[1].isalpha и длина scnd_raw[0] больше 2 и cnd_raw[0] больше 2
-#                 res = 'Email' # то это Email
-#
-# else:  # в ином случае
-#     raw_user_input = (user_input)
-#
-#     for letter in user_input: # для каждой буквы. что мы ввели от пользователя
-#         if letter in signs:   # если буква содержит ()-" "
-#             raw_user_input = raw_user_input.replace(letter, '') # то заменяем на ""
-#     if any([
-#         len(raw_user_input) == 13 and raw_user_input[0:3] == ["+", "3", "8", "0"] and raw_user_input[4:].isdigit(),
-#         len(raw_user_input) == 10 and raw_user_input[0:2] == ["0", "6", "6"], ["0", "9", "9"], ["0", "5", "0"],
-#         ["0", "9", "5"], ["0", "6", "3"], ["0", "9", "3"], ["0", "7", "3"], ["0", "6", "7"], ["0", "6", "8"],
-#         ["0", "9", "6"], ["0", "9", "7"], ["0", "9", "8"], ["0", "3", "9"] and raw_user_input[3:].isdigit()
-#     ]):
-# # если длина = 13 и начало начинается с + и от 1 до конца у него цыфры
-# # если длина = 10 и вся строка цифры
-#         res = 'Phone' # то результат Phone
-#
-# if res:  # если есть результат и она не пустая
-#     print(f'\"{user_input}\" is {res}')
-# else:    # в ином случае
-#     print('WHAT?????')  # Если введеная строка будет пустая будет False, и выведет WHAT?????
+''' Задание №3 Обеспечить пользователю возможность повторять операцию - спрашивать, хочет ли он продолжить (Y/N)...
+               (вы уже делали такое в дз №3, задание 3, адаптируйте свой код под это) '''
+# Мы в предыдущем задание расмотревали все варианты.
+
+while True:
+    operation = input("Хочешь ли ты повторить операцию? (Y/N): ")
+    if operation == "Y":
+        operation = operation
+    else:
+        if operation == "N":
+            print("Программа завершина")
+            break
+        else:
+            while True:
+                if operation != "Y" and "N":
+                    operation = input("Хочешь ли ты повторить операцию??? (Y/N): ")
+                    while True:
+                        operation = input("Хочешь ли ты повторить операцию?????? (Y/N): ")
+                        if operation == "Y":
+                            operation = operation
+                        else:
+                            if operation == "N":
+                                print("Программа завершина")
+                                break
+                break
+    break
+
+
+while True:
+    print('Do some actions')
+
+    answer = None
+    while True:
+        operation = input("Хочешь ли ты повторить операцию? (Y/N): ")
+        if operation not in 'YN':
+            print('Wrong answer')
+            continue
+        answer = operation == 'Y'
+
+    if not answer:
+        break
+
+''' Задание №4* Пользователь вводит несколько чисел через пробел. Определить все ли числа веденные пользователем 
+                уникальны (нет ли среди чисел одинаковых по значению). Составить из уникальных чисел массив их квадратов.
+                Составить из неуникальных чисел массив их кубов. '''
+
+
+numbers = list(map(int, input("insert the number: ").split()))
+
+for number in numbers:
+    if numbers.count(number) == 1:
+        print(number **2)
+    if numbers.count(number) != 1:
+        print(number **3)

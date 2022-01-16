5cccbc9d90585d8a6b947af796631cf2c6d39daa
Andrii Kozlenko <mister.andrey7777@gmail.com>
2022-01-16 13:04:24 +0200

    homework5 corrected.py

--- homework 5.py
+++ homework 5.py
@@ -1,6 +1,6 @@
 ''' 1. Задание. Пользователь вводит строку. Преобразовать её в tuple сотоящий из слов.'''
 
-user_input = input("Enter the string: " )
+user_input = input("Enter the string: " ).split()
 
 user_input1 = tuple(user_input)
 
@@ -10,37 +10,45 @@ print(user_input1)
 произведение, если строками - собрать в одну строку и вернуть, если первая строка, а вторая нет - вернуть словарь, 
  в котором ключ - первый аргумент, значение - второй.'''
 
-def function (a,b):
-    if type(a)==int and type(b)==int:
-        return (a*b)
-    elif type(a)==str and type(b)==str:
-        return (a+b)
-    elif type(a)==str and type(b)==float:
-        slovary={a:b}
+def function(a, b):
+    if type(a) == int and type(b) == int or type(a) == float and type(b) == float:
+        return a * b
+    elif type(a) == str and type(b) == str:
+        return a + b
+    elif type(a) == str and type(b) == float:
+        slovary = {a: b}
         return slovary
     else:
         return "non-compliance by the condition of the task"
-print(function(33,33))
-print(function("And","rii"))
-print(function("my temperatura", 36.6))
+
+
+print(function(3.3, 3.3))
+print(function("And", "rii"))
+# print(function("my temperatura", 36.6))
+
 
 # ещё пример с отдельными функциями
 
 def foo(arg1, arg2):
     res = arg1 * arg2
-    return int(res)
+    return float(res)
+
 
 print(foo(5, 7))
+
 
 def foo(arg3, arg4):
     res = arg3 + arg4
     return str(res)
 
+
 print(foo("Kozl", "enko"))
 
+
 def foo(arg5, arg6):
-    res = {arg5:arg6}
+    res = {arg5: arg6}
     return str(arg5), int(arg6)
+
 
 print(foo("Happy New Year", 2222))
 
@@ -49,34 +57,39 @@ print(foo("Happy New Year", 2222))
 "ipartner":38.988, "sota":37.720}, 
 написать фукцию возвращающую список имен продавцов, чьи цены попадают в диапазон (from_price, to_price). '''
 
-def list_of_seller_names (dictionary, a, b):
+def list_of_seller_names(dictionary, a, b):
     return [dict_item[0] for dict_item in dictionary.items() if a <= dict_item[1] <= b]
 
-iphone_dictionary = {"citrus":47.999, "istudio":42.999, "moyo":49.999, "royal-service":37.245, "buy.ua":38.324,
-"g-store":37.166, "ipartner":38.988, "sota":37.720}
 
-print(list_of_seller_names (iphone_dictionary, 37.000, 45.000))
+iphone_dictionary = {"citrus": 47.999, "istudio": 42.999, "moyo": 49.999, "royal-service": 37.245, "buy.ua": 38.324,
+                     "g-store": 37.166, "ipartner": 38.988, "sota": 37.720}
+
+print(list_of_seller_names(iphone_dictionary, 37.000, 45.000))
 
 ''' 4. Задание. *Реализовать в виде функции ДЗ №3-3 (Попросить пользователя ввести с клавиатуры строку и вывести её на 
 экран. Спросить у пользователя хочет ли он повторить операцию (Y/N)?. Повторять операцию если (Y), завершить выполнения 
 если (N), если ответ не Y или N - переспрашивать пока не введет Y или N.) Получение ответа на вопрос (Y/N)? реализовать 
 отдельной функцией и встроит в основную. '''
 
-def play_operation():
-    def play_operation1():
 
-        while True:
-            operation = input("Хочешь ли ты повторить операцию? (Y/N): ")
+def play_operation():
+
+    while True:
+        operation = input("Хочешь ли ты повторить операцию? (Y/N): ")
+
+        def play_operation1():
+
             if operation == "Y":
                 print("Program continues")
             else:
                 if operation == "N":
                     print("Program completed")
-                    break
                 else:
                     if operation != "Y" and "N":
-                        operation = input("Хочешь ли ты повторить операцию??? (Y/N): ")
-    play_operation1()
+                        print('Wrong answer')
+
+        play_operation1()
+
 
 play_operation()
 
@@ -90,11 +103,14 @@ play_operation()
 punktuation:list уникальных знаков препинания
 } '''
 
-# здесь немного не могу понять, как это всё правильно реализовать
-# получилось сыровато
+
+# иного вариана не получается придумать
 
 def foo():
-    words = 'python is the best programming software'.split(' ')
+    world = 'python is the best. best programming software'
+    words = world.split(' ')
+    a = world.count(" ")
+    b = world.count(".")
     longer = max(map(len, words))
 
     result = {}
@@ -103,5 +119,8 @@ def foo():
 
     for word in words:
         result[len(word) - 1].append(word)
+        result[(a)].append(" ")
+        result[(b)].append(" . ")
     print(result)
+
 foo()

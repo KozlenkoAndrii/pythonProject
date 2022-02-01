41af5c57cbeeb413b1c83f60fbec4588347d06f2
Andrii Kozlenko <mister.andrey7777@gmail.com>
2022-02-02 00:28:02 +0200

    homework6 corrected.py

--- homework6.py
+++ homework6.py
@@ -5,65 +5,72 @@
 
 import random
 
-game = input("Good day! Specify a range of numbers! In the format int-int: ")
-int_begin, int_end = map(int, game.split("-"))
+def foo():
+    game = input("Good day! Specify a range of numbers! In the format int-int: ")
+    int_begin, int_end = map(int, game.split("-"))
+    random_number = random.randint(int_begin, int_end)
 
-random_number = random.randint(int_begin, int_end)
-
-while random_number != game:
-    number = int(input("What number?: "))
-    if number == random_number:
-        operation = input("Хочешь ли ты повторить игру? (Y/N): ")
-        if operation == "Y":
-            operation = operation
-        else:
-            if operation == "N":
-                break
+    def foo1():
+        while random_number != game:
+            number = int(input("What number?: "))
+            if number == random_number:
+                operation = input("Хочешь ли ты повторить игру? (Y/N): ")
+                if operation == "Y":
+                    operation = operation
+                else:
+                    if operation == "N":
+                        break
+    foo1()
+foo()
 
 # ещё вариант
 
-import random
+def foo():
 
-game = input("Good day! Specify a range of numbers! In the format int-int: ")
-int_begin, int_end = map(int, game.split("-"))
+    game = input("Good day! Specify a range of numbers! In the format int-int: ")
+    int_begin, int_end = map(int, game.split("-"))
 
-random_number = random.randint(int_begin, int_end)
+    random_number = random.randint(int_begin, int_end)
 
-while random_number != game:
-    number = int(input("What number?: "))
-    if number == random_number:
-        operation = input("Хочешь ли ты повторить игру? (Y/N): ")
-        if not operation == "Y":
-            break
+    def foo1():
+        while random_number != game:
+            number = int(input("What number?: "))
+            if number == random_number:
+                operation = input("Хочешь ли ты повторить игру? (Y/N): ")
+                if not operation == "Y":
+                    break
+    foo1()
+foo()
 
 ''' Задание №3 Добавить в задание №2 счетчик попыток. Кроме диапазона чисел пользователь вводит количество попыток, 
 за которые он может угадать число. На каждом шаге сообщайте пользователю сколько попыток у него осталось. если 
 пользователь не смог угадать за отведенное количество попыток сообщить ему об окончании (Looser!). Cпросить у 
 пользователя, хочет ли он повторить игру (Y/N). Если Y повторить.'''
 
-import random
 
-while True:
+def foo():
 
-    game = input("Good day! Specify a range of numbers! In the format int-int : ")
-    attempts = int(input("Enter the number of attempts: "))
-    int_begin, int_end = map(int, game.split("-"))
+    while True:
 
-    random_number = random.randint(int_begin, int_end)
-    for i in range(attempts):
-        number = int(input("What number?: "))
-        attempts -= 1
-        print("Попыток осталось", attempts)
-        if number == random_number:
-            print("Guessed!")
-            break
+        game = input("Good day! Specify a range of numbers! In the format int-int : ")
+        attempts = int(input("Enter the number of attempts: "))
+        int_begin, int_end = map(int, game.split("-"))
 
-    else:
-        print("Looser!")
-        print("Hidden number - {}".format(random_number))
+        random_number = random.randint(int_begin, int_end)
+        for i in range(attempts):
+            number = int(input("What number?: "))
+            attempts -= 1
+            print("Попыток осталось", attempts)
+            if number == random_number:
+                print("Guessed!")
+                break
+        else:
+            print("Looser!")
+            print("Hidden number - {}".format(random_number))
 
-    operation = input(f"Хочешь ли ты повторить игру? (Y/N): ")
+            operation = input(f"Хочешь ли ты повторить игру? (Y/N): ")
 
-    if operation not in ("Y"):
-        print("Game over!")
-        break
+            if operation not in ("Y"):
+                print("Game over!")
+                break
+foo()
